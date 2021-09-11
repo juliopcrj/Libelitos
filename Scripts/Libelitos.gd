9c342f2da31c7a40f0586173d8ca1001b1bdf305
Julio Pinto Coelho <juliopcrj@gmail.com>
2021-09-11 19:20:17 -0300

    Adicionando o marimba, e tiros matam

--- Scripts/Libelitos.gd
+++ Scripts/Libelitos.gd
@@ -1,8 +1,18 @@
 extends KinematicBody2D
 
 var Projectile = preload("res://Scenes/Projectile.tscn")
+var Shot_Sound = preload("res://Sounds/shot.wav")
 
+const speed = Vector2()
+onready var bulletTimer = get_node("bulletTimer")
+var can_shoot = true
 
+func _ready():
+	$AudioStreamPlayer2D.stream = Shot_Sound
+	$AudioStreamPlayer2D.volume_db = -10
+	speed.x = 200
+	speed.y = 200
+	
 
 func _physics_process(delta):
 	process_inputs()
@@ -12,14 +22,24 @@ func process_inputs():
 		fire()
 	var horizontal = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	var vertical = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	horizontal = horizontal * speed.x
+	vertical = vertical * speed.y
 	move_and_slide(Vector2(horizontal, vertical))
 
 func fire():
-	var _p = Projectile.instance()
-	_p.collision_layer # = 2
-	_p.set_collision_layer_bit(0b0100)
-	_p.set_collision_mask_bit(0b0010)
-	var main = get_tree().current_scene
-	_p.global_position = self.global_position
-	main.add_child(_p)
+	if(can_shoot):
+		$AudioStreamPlayer2D.play(0)
+		var _p = Projectile.instance()
+		_p.set_player_fire()
+		var main = get_tree().current_scene
+		_p.global_position = self.global_position
+		main.add_child(_p)
+		can_shoot = false
+		bulletTimer.set_wait_time(.2)
+		bulletTimer.start()
 
+
+
+func _on_bulletTimer_timeout():
+	can_shoot = true
+	bulletTimer.stop()
--- Scripts/Projectile.gd
+++ Scripts/Projectile.gd
@@ -12,6 +12,16 @@ func prepare(x, y):
 	velocity.y = y
 
 func _physics_process(delta):
-	if(position.y < 0):
+	if(position.y < 0) || get_slide_count() == 1:
 		queue_free()
 	move_and_slide(velocity)
+	#for i in range(get_slide_count() -1 ):
+	#	print(get_slide_collision(i))
+		
+func set_player_fire():
+	set_collision_layer_bit(2,true)
+	set_collision_mask_bit(1, true)
+
+func set_enemy_fire():
+	set_collision_layer_bit(3,true)
+	set_collision_mask_bit(0, true)
