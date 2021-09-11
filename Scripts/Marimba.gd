4ab3ea56f5f678d0699b770775104a4779c1a572
Julio Pinto Coelho <juliopcrj@gmail.com>
2021-09-11 19:46:35 -0300

    Stuff I guess

---
+++ Scripts/Marimba.gd
@@ -0,0 +1,49 @@
+extends KinematicBody2D
+
+
+var Projectile = preload("res://Scenes/Projectile.tscn")
+onready var shotTimer = get_node("ShotTimer")
+
+
+const SHOTS_PER_SECOND = 1.5
+const BULLET_SPEED = 200
+
+var life:int
+var can_shoot:bool
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	can_shoot = true
+	life = 2
+	pass # Replace with function body.
+
+func shoot():
+	if can_shoot:
+		var _p = Projectile.instance()
+		_p.prepare(0, BULLET_SPEED)
+		_p.global_position = self.global_position
+		_p.set_enemy_fire()
+		var main = get_tree().current_scene
+		main.add_child(_p)
+		shotTimer.set_wait_time(1/SHOTS_PER_SECOND)
+		shotTimer.start()
+		can_shoot = false
+
+func _process(delta):
+	move_and_slide(Vector2.ZERO)
+	shoot()
+	if(get_slide_count()):
+		if(get_slide_collision(0).collider.name != "Libelitos"):
+			take_damage()
+	
+func take_damage():
+	life -= 1
+	if life == 0:
+		queue_free()
+	pass
+
+
+func _on_ShotTimer_timeout():
+	can_shoot = true
+	shotTimer.stop()
